e10d5391ada2755e2917ea2d212b51308ff415e4
manyyuri <327035324@qq.com>
2025-09-26 22:30:27 +0800

    refactor: 删除多余样式

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { Menu } from "antd";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
         身材管理
-        <Menu items={[{key: 1, label: '今日计划'},{key: 2, label: '减脂'},{key: 3, label: '增肌'},{key: 4, label: '软开'},{key: 5, label: '体态'},{key: 6, label: '资料'},]}/>
+        <Menu defaultValue={1} items={[{key: 1, label: '今日计划'},{key: 2, label: '减脂'},{key: 3, label: '增肌'},{key: 4, label: '软开'},{key: 5, label: '体态'},{key: 6, label: '资料'},]}/>
     </div>
   );
 }
